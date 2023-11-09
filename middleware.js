a1b861cb601d74b55060412495b75b307d1b52de
mukesh kumar <mkmehta2041@gmail.com>
2023-11-09 16:53:18 +0530

    middleware to check admins

--- middleware.js
+++ middleware.js
@@ -1,4 +1,24 @@
-export { default } from "next-auth/middleware";
+import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+
+export default withAuth(
+  function middleware(req) {
+    console.log(req.nextUrl.pathname);
+    console.log(req.nextauth.token.role);
+
+    if (
+      req.nextUrl.pathname.startsWith("/create-user") &&
+      req.nextauth.token.role !== "admin"
+    ) {
+      return NextResponse.rewrite(new URL("/denied", req.url));
+    }
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => !!token,
+    },
+  }
+);
 
 export const config = {
   matcher: ["/create-user"],
